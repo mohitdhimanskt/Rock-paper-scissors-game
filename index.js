cc67365fdb67e4c474563bcbc3458c526679b6df
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-27 10:42:03 +0530

    Thirteencommit

--- index.js
+++ index.js
@@ -26,3 +26,27 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
+function getWinner(p, c) {
+    if (p === c) {
+      return 'draw';
+    } else if (p === 'rock') {
+      if (c === 'paper') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    } else if (p === 'paper') {
+      if (c === 'scissors') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    } else if (p === 'scissors') {
+      if (c === 'rock') {
+        return 'computer';
+      } else {
+        return 'player';
+      }
+    }
+  }
+  
